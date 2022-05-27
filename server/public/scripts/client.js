767ac0979f11268a7ee85652660a30cfa37a7c39
Jean-Luc LaCosse <jeanlacosse@gmail.com>
2022-05-27 08:57:04 -0500

    completed GET for client side only

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,17 +2,53 @@ $(document).ready(function () {
     console.log('jQuery sourced.');
     refreshTodo();
     addClickHandlers();
-  });
-  
-  function addClickHandlers() {
+});
+
+function addClickHandlers() {
     $('#submitBtn').on('click', handleSubmit);
-//   create handlers for delete and completed item
-  }
+    //   create handlers for delete and completed item
+}
 
-  function handleSubmit(){
+function handleSubmit() {
+    //   on submit (POST) going to grab input value into a variable, will run addTodo with that variable
+    console.log('button working')
+}
 
-  }
+function refreshTodo() {
+    // on page start (GET) will garb all items from server and render to DOM with a seperate function
+    console.log('refresh working')
 
-  function refreshTodo(){
-      
-  }
+    $.ajax({
+        type: 'GET',
+        url: '/todo'
+    })
+        .then((response) => {
+            console.log(response);
+            renderTodo(response);
+        })
+        .catch(function (error) {
+            console.log('error in GET', error);
+        });
+}
+
+function renderTodo(todos) {
+    $('#listArea').empty();
+
+    // loop over todo list and append to the DOM
+    for (let todo of todos) {
+        // create a delete button and a completed button on append
+        // give data for id and boolean for if completed to use in the DELETE and PUT
+        $('#listArea').append(`
+        <tr 
+        data-todo-id="${todo.id}"
+        data-todo-completed="${todo.completed}"
+        >
+          <td>${todo.list_item}</td>
+          <td>${todo.completed}</td>
+          <td><button class="deleteBtn">Delete</button></td>
+          <td><button class="readBtn">Read?</button></td>
+        </tr>
+        `)
+    }
+
+}
