b2c7fd33c61afd6bcf724878e9dff4202adc7d38
Jean-Luc LaCosse <jeanlacosse@gmail.com>
2022-05-28 12:56:14 -0500

    changed sql

--- database.sql
+++ database.sql
@@ -1,7 +1,8 @@
 CREATE TABLE "todo-list" (
 	"id" SERIAL PRIMARY KEY,
 	"list_item" VARCHAR (250) NOT NULL,
-	"completed" BOOLEAN DEFAULT FALSE
+	"completed" BOOLEAN DEFAULT FALSE,
+	"time_completed" VARCHAR(250)
 );
 
 INSERT INTO "todo-list"
